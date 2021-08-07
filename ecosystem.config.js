8fc6177daeabab61c09b67bf08e17231e3b32d2c
nikolaj <lopudesigns@gmail.com>
2021-08-07 19:07:29 +1000

    dev

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,16 @@ module.exports = {
   apps : [
 		{
 			name: "dev-muppets",
-			script: 'node/index.js --level dev',
+			script: 'node/index.js',
+			args: "--level dev",
 			watch: ['node', "node/*/node_modules", "node/**/node_modules", "node/node_modules"],
 			ignore_watch: [],
 			node_args: "--trace-warnings"
 		},
 		{
 			name: "prod-muppets",
-			script: 'node/index.js --level prod',
+			script: 'node/index.js',
+			args: "--level prod"
 		}
 	],
   deploy : {
