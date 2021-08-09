d06c9554aed65586f3c449e6ff2b7d9c36c98914
nikolaj <lopudesigns@gmail.com>
2021-08-09 22:49:17 +1000

    Deletes old products that were not found in this scrape

--- node/muppets.js
+++ node/muppets.js
@@ -1,13 +1,19 @@
 global.env = require('minimist')(process.argv.slice(2))
 global.Rollbar = require('rollbar')
 global.secrets = require('secrets')
-global.rollbar = new Rollbar({
-  accessToken: global.secrets.rollbar.key,
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-	verbose: true,
-	environment: global.env.level == 'prod' ? 'production' : 'development'
-})
+if (global.env.level == "prod") {
+	global.rollbar = new Rollbar({
+		accessToken: global.secrets.rollbar.key,
+		captureUncaught: true,
+		captureUnhandledRejections: true,
+		verbose: true,
+		environment: global.env.level == 'prod' ? 'production' : 'development'
+	})
+}
+// catches all uncaught errors so process never dies
+process.on("uncaughtException", function (err) {
+  console.log("[muppets][error] Caught exception: ", err);
+});
 require('functions')["splash.js"]()
 global.logging = {
 	mongoose: {
@@ -19,20 +25,20 @@ global.logging = {
 	}
 }
 
-console.log(`Started Muppets, env: `, global.env)
+console.log(`[muppets][info] Started Muppets, env: `, global.env)
 
 let muppets = require('muppets')()
 let scripts = require('scripts')
 let minutes = 5 
 
 if(global.logging && global.logging.timing){
-	console.log(`running scripts now, and then every ${minutes} minutes`)
+	console.log(`[muppets][info] running scripts now, and then every ${minutes} minutes`)
 }
 let run = async ()=>{
 	
 	await scripts({muppets}).catch(err=>console.error(err))
 	global.scriptsInterval = setInterval(async ()=>{
-		await scripts({muppets}).catch(err=>{console.error('something went wrong running the first scripts({muppets}), ', err)})
+		await scripts({muppets}).catch(err=>{console.error('[muppets][error] something went wrong running the first scripts({muppets}), ', err)})
 	}, minutes * 1000 * 60)
 
 }
